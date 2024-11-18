0d755d0fbd42dbd948e74eeba3271380795c137e
yunho <nam100400@naver.com>
2024-11-18 17:36:26 +0900

    목록 구현 완료

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,8 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 class Say{
 
@@ -31,26 +33,52 @@ class Say{
 
 public class App {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("== 명언 앱 ==");
-        int cnt = 1;
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final List<Say> sayList = new ArrayList<>();
+    private static int cnt = 1;
 
-        while (true){
+    private static void add() throws IOException {
+        System.out.print("명언 : ");
+        String text = br.readLine();
+        System.out.print("작가 : ");
+        String writer = br.readLine();
+
+        Say say = new Say(text, writer, cnt);
+        sayList.add(say);
+        System.out.println(cnt + "번 명언이 등록되었습니다.");
+        cnt++;
+    }
+
+    private static void list(){
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("-----------------");
+        for (Say say : sayList) {
+            System.out.println(say.getNumber() + " / " +
+                    say.getWriter() + " / " +
+                    say.getText()
+            );
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("== 명언 앱 ==");
+
+        boolean exit = true;
+
+        while (exit){
             System.out.print("명령) ");
             String s = br.readLine();
 
-            if(s.equals("종료"))
-                break;
-            else if(s.equals("등록")){
-                System.out.print("명언 : ");
-                String text = br.readLine();
-                System.out.print("작가 : ");
-                String writer = br.readLine();
-
-                Say say = new Say(text, writer, cnt);
-                System.out.println(cnt + "번 명언이 등록되었습니다.");
-                cnt++;
+            switch (s) {
+                case "종료" -> {
+                    exit = false;
+                }
+                case "등록" -> {
+                    add();
+                }
+                case "목록" ->{
+                    list();
+                }
             }
         }
         br.close();
