2c5f3157b30837fd4b070fc439456ba18719a9de
yunho <nam100400@naver.com>
2024-11-18 17:32:21 +0900

    ë²번호 노출 및 증가 구현

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,12 +6,14 @@ import java.io.InputStreamReader;
 
 class Say{
 
+    private final int number;
     private final String text;
     private final String writer;
 
-    public Say(String text, String writer) {
+    public Say(String text, String writer, int number) {
         this.text = text;
         this.writer = writer;
+        this.number = number;
     }
 
     public String getText() {
@@ -21,17 +23,21 @@ class Say{
     public String getWriter() {
         return writer;
     }
+
+    public int getNumber() {
+        return number;
+    }
 }
 
 public class App {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         System.out.println("== 명언 앱 ==");
+        int cnt = 1;
+
         while (true){
             System.out.print("명령) ");
-
             String s = br.readLine();
 
             if(s.equals("종료"))
@@ -42,10 +48,11 @@ public class App {
                 System.out.print("작가 : ");
                 String writer = br.readLine();
 
-                Say say = new Say(text, writer);
+                Say say = new Say(text, writer, cnt);
+                System.out.println(cnt + "번 명언이 등록되었습니다.");
+                cnt++;
             }
         }
-
         br.close();
     }
 }
